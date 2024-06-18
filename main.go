5b4be0f13b1aae724688f4d155c4de71fc6ca120
Juan-Alves-1 <116723299+Juan-Alves-1@users.noreply.github.com>
2024-06-18 02:01:06 -0300

    Final adjustments for Heroku deployment

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"on-page-seo/database"
 	"on-page-seo/src/handler"
 
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,5 +19,9 @@ func main() {
 	r.GET("/url-checker", handler.ShowChecker)
 	r.POST("/url-checker/analyze", handler.UrlCheckerAnalysis)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
