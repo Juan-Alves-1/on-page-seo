a75dc6195309d5fb0c4ca3d4c327c79d54866359
Juan-Alves-1 <116723299+Juan-Alves-1@users.noreply.github.com>
2024-06-24 16:46:06 -0300

    Update database configuration

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
@@ -12,22 +13,33 @@ var DB *sql.DB
 
 func InitDB() {
 	var err error
-	dsn := os.Getenv("DATABASE_URL")
+
+	// Get the database connection information from environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+
+	// Form the DSN (Data Source Name)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbName)
+
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
 	}
 
+	// Verify the connection is valid
 	if err = DB.Ping(); err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
 
-	// if err = AutoMigrate(); err != nil {
-	// 	return err
-	// }
-
 	log.Println("Database connection established")
 }
+
+// if err = AutoMigrate(); err != nil {
+// 	return err
+// }
 
 // func AutoMigrate() error {
 // 	DB.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
